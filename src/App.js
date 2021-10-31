7e2b28a1b5e1a8564bf0d81b2588d54b173b2693
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2021-10-31 11:12:48 +0900

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React, { useState } from 'react';
 import './reset.css';
 import './style.scss';
 
 function App() {
+  const [word, setWord] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [archives, setArchives] = useState([]);
+
+  const getWord = (e) => {
+    setWord(e.target.value);
+  };
+
+  const addTasks = () => {
+    setTasks([...tasks, word]);
+    setWord("")
+  }
+
+  const addArchive = (i) => {
+    setArchives([...archives, tasks[i]])
+    const newTasks = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks)
+  }
+
+  const deleteTask = (i) => {
+    const newTasks = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks)
+  }
+
+  const deleteArchive = (i) => {
+    const newArchives = [...archives];
+    newArchives.splice(i, 1);
+    setArchives(newArchives)
+  }
+
   return (
     <div>
       <main className="container">
@@ -9,8 +42,8 @@ function App() {
 
         <div className="add">
           <form>
-            <input type="text"/>
-            <button type="button">追加</button>
+            <input type="text" value={word} onChange={getWord}/>
+            <button type="button" onClick={addTasks}>追加</button>
           </form>
         </div>
 
@@ -18,64 +51,33 @@ function App() {
           <div className="todo">
             <h2>todo</h2>
             <ul className="todo__list">
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="move"></button>
-                  <button className="delete"></button>
-                </div>
-              </li>
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="move"></button>
-                  <button className="delete"></button>
-                </div>
-              </li>
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="move"></button>
-                  <button className="delete"></button>
-                </div>
-              </li>
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="move"></button>
-                  <button className="delete"></button>
-                </div>
-              </li>
+              {tasks.map((task, i) => {
+                return (
+                  <li className="todo__list__item" key={i}>
+                    <p className="todo__list__item--title">{task}</p>
+                    <div className="todo__list__item__btn">
+                      <button className="move" onClick={() => addArchive(i)}></button>
+                      <button className="delete" onClick={() => deleteTask(i)}></button>
+                    </div>
+                  </li>
+                )
+              })}
             </ul>
           </div>
 
           <div className="todo">
             <h2>done</h2>
             <ul className="todo__list">
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="delete"></button>
-                </div>
-              </li>
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="delete"></button>
-                </div>
-              </li>
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="delete"></button>
-                </div>
-              </li>
-              <li className="todo__list__item">
-                <p className="todo__list__item--title">タスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスクタスク</p>
-                <div className="todo__list__item__btn">
-                  <button className="delete"></button>
-                </div>
-              </li>
+              {archives.map((archive, i) => {
+                return (
+                  <li className="todo__list__item" key={i}>
+                    <p className="todo__list__item--title">{archive}</p>
+                    <div className="todo__list__item__btn">
+                      <button className="delete" onClick={() => deleteArchive(i)}></button>
+                    </div>
+                  </li>
+                )
+              })}
             </ul>
           </div>
         </div>
